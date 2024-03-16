a871506c2e78636643f8487f4d9060bffb8f5c97
Uhmpasterig <805luca805@gmail.com>
2024-03-17 00:28:44 +0100

    feat: Improve HighlightedText component and update metadata in RootLayout
    - Refactored HighlightedText component to fix rendering issues and improve readability
    - Updated openGraph metadata in RootLayout to include additional information and an image for better social sharing.

--- components/containers/LayoutContainer.tsx
+++ components/containers/LayoutContainer.tsx
@@ -12,8 +12,12 @@ export const LayoutContainer = ({ children }: { children: React.ReactNode }) => 
     <html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+{/*         <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" /> */}
       </Head>
-      <body className={cn(poppins.className, "w-screen overflow-x-hidden min-h-screen")}>
+      <body className={cn(poppins.className, 'w-screen overflow-x-hidden min-h-screen')}>
         <ThemeProvider attribute="class" storageKey="theme" defaultTheme="dark">
           {children}
         </ThemeProvider>
