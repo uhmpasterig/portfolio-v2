0820108adc605b9250bb8c69d83b7303e5cf98d7
Luca <805luca805@gmail.com>
2024-03-18 11:17:57 +0100

    refactor: Refactor ListSection component to improve performance
    - Replaced async function with regular function for ListSection component
    - Created ListSectionWrapper component to handle media queries
    - Improved media query handling for responsive design
    - Removed unnecessary dependencies and optimized code structure

--- components/sections/List.tsx
+++ components/sections/List.tsx
@@ -2,25 +2,17 @@
 import { Grid } from '@/components/Grid';
 import { fetchProjects, getImageUrl } from '@/lib/utils';
 import { ListItem, ListItemProps } from '@/components/ListItem';
-import { useRef } from 'react';
-import { useScroll, useTransform, motion } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
+import { useScroll, useTransform, motion, MotionValue } from 'framer-motion';
 
 type ListSectionProps = {
   title: string;
   items: ListItemProps[];
 };
 
-export const ListSection = async ({ title, items }: ListSectionProps) => {
-  const sectionRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: sectionRef,
-    offset: ['end end', 'start start'],
-  });
-
-  const opacity = useTransform(scrollYProgress, [0, 0.25, 0.75, 1], [0.5, 1, 1, 0.5]);
-  const scale = useTransform(scrollYProgress, [0, 0.25, 0.75, 1], [0.97, 1, 1, 0.97]);
+export const ListSection = ({ title, items }: ListSectionProps) => {
   return (
-    <motion.section ref={sectionRef} style={{ opacity, scale }} className="w-full">
+    <ListSectionWrapper>
       <h2 className="text-3xl font-bold mb-4 md:text-left text-center ">{title}</h2>
       <Grid>
         {items.map((item, i) => (
@@ -34,6 +26,51 @@ export const ListSection = async ({ title, items }: ListSectionProps) => {
           />
         ))}
       </Grid>
+    </ListSectionWrapper>
+  );
+};
+
+const ListSectionWrapper = ({ children }: Props.HasChildren) => {
+  const sectionRef = useRef(null);
+  const isMd = useMediaQuery('md');
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['end end', 'start start'],
+  });
+
+  let opacity = useTransform(scrollYProgress, [0, 0.25, 0.75, 1], [0.5, 1, 1, 0.5]);
+  let scale = useTransform(scrollYProgress, [0, 0.25, 0.75, 1], [0.97, 1, 1, 0.97]);
+  if (!isMd) {
+    let motionValue = new MotionValue();
+    motionValue.set(1);
+    opacity = motionValue.get();
+    scale = motionValue.get();
+  }
+  return (
+    <motion.section ref={sectionRef} style={{ opacity, scale }} className="w-full sm:opacity-100">
+      {children}
     </motion.section>
   );
+};
+
+export const sizes = {
+  sm: '640px',
+  md: '768px',
+};
+
+const useMediaQuery = (screen: keyof typeof sizes) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const query = `(min-width: ${sizes[screen]})`;
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    window.addEventListener('resize', listener);
+    return () => window.removeEventListener('resize', listener);
+  }, [matches, screen]);
+
+  return matches;
 };
